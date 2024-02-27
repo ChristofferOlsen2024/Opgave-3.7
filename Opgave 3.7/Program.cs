165118f59d04c690e19eb2cdc5b699245e383532
Christoffer Olsen <chris70944@gmail.com>
2024-02-27 10:41:08 +0100

    Rettet lidt

--- Opgave 3.7/Program.cs
+++ Opgave 3.7/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hvordan er vejret udenfor?");
 var vejr = Console.ReadLine();
+Console.Clear();
 
 Console.WriteLine("Hvor mange grader er det udenfor?");
 int grader = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
 
-Console.WriteLine($"Det er {vejr} og {grader} uden for");
+Console.WriteLine($"Det er {vejr} og {grader} grader udenfor");
 
