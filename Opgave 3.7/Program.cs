b59c6b5ba7c838d02a251eca7ef6afd7371fe77f
Christoffer Olsen <chris70944@gmail.com>
2024-02-27 10:38:23 +0100

    Add project files.

---
+++ Opgave 3.7/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hvordan er vejret udenfor?");
+var vejr = Console.ReadLine();
+
+Console.WriteLine("Hvor mange grader er det udenfor?");
+int grader = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Det er {vejr} og {grader} uden for");
+
